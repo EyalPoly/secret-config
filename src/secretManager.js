1f5d9581d75f3b262b6d5027fbd57a2315e7d985
Eyal Politansky <eyalpolitansky@Eyals-MacBook-Pro.local>
2025-01-23 17:59:54 +0200

    first commit

---
+++ src/secretManager.js
@@ -0,0 +1,33 @@
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
+
+class SecretManager {
+  constructor(options = {}) {
+    this.client = new SecretManagerServiceClient();
+    this.cache = new Map();
+    this.projectId = options.projectId || process.env.GOOGLE_CLOUD_PROJECT_ID;
+  }
+
+  async getSecret(secretName, maxAge = 3600000) {
+    const cached = this.cache.get(secretName);
+    if (cached && Date.now() - cached.timestamp < maxAge) {
+      return cached.value;
+    }
+
+    try {
+      const name = `projects/${this.projectId}/secrets/${secretName}/versions/latest`;
+      const [version] = await this.client.accessSecretVersion({ name });
+      const value = version.payload.data.toString("utf8");
+
+      this.cache.set(secretName, {
+        value,
+        timestamp: Date.now(),
+      });
+
+      return value;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+module.exports = SecretManager;
